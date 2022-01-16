a3be46970237b8e3067ede322d390afec47a4e75
Aidan <aidanenseleit98@gmail.com>
2022-01-16 23:38:18 +1100

    setup super basic list item creation

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full border border-solid border-black p-5 h-20" v-text="'Item ' + number"></div>
+</template>
+
+<script>
+  export default {
+    props: {
+      number: {
+        type: Number,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="flex flex-col justify-center items-center h-full">
     <h1>Home</h1>
-    <p v-text="this.$store.state.count"></p>
+    <p v-text="counter"></p>
+    <button @click="this.$store.commit('increment')">Increment</button>
+    <div v-for="num in counter" :key="num">
+      <TodoItem :number="num"></TodoItem>
+    </div>
   </div>
 </template>
 <script>
-  export default {};
+  import {computed} from '@vue/reactivity';
+  import TodoItem from '../components/TodoItem.vue';
+  export default {
+    components: {TodoItem},
+    computed: {
+      counter() {
+        return this.$store.state.count;
+      },
+    },
+  };
 </script>
 <style lang=""></style>
